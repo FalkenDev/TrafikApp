5d13e168386cf66fb0f5b43f78023b8645691152
“FalkenDev” <kafa21@student.bth.se>
2022-05-11 01:21:09 +0100

    Added styles for the app

---
+++ styles/base.js
@@ -0,0 +1,24 @@
+export const container = {
+    flex: 1,
+    backgroundColor: '#1a1a1a',
+}
+
+export const image = {
+    flex: 1,
+    justifyContent: "center",
+    height: 700,
+    margin: 0,
+    padding: 0,
+}
+
+export const home = {
+    flex: 1,
+    backgroundColor: '#1a1a1a',
+}
+
+export const base = {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingLeft: 12,
+    paddingRight: 12,
+}
---
+++ styles/typography.js
@@ -0,0 +1,50 @@
+export const title = {
+    color: '#fff',
+    fontSize: 30,
+    textAlign: 'center',
+};
+
+export const header2 = {
+    color: '#333',
+    fontSize: 24,
+    borderBottomWidth: 2,
+    borderLeftColor: 'black',
+    marginTop: 10,
+}
+
+export const label = {
+    color: '#333',
+    fontSize: 20,
+    marginTop: 10,
+    marginLeft: 10,
+}
+export const labelDate = {
+    color: '#333',
+    fontSize: 16,
+    marginTop: 10,
+}
+
+export const details_text = {
+    fontSize: 20,
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 10,
+}
+
+export const details_highlight = {
+    fontSize: 20,
+    marginTop: 10,
+    borderBottomWidth: 2,
+    width: 92,
+    marginLeft: 10,
+}
+
+export const warning = {
+    color: '#FF0000',
+    fontSize: 18,
+    marginTop: 10,
+    textAlign: 'center',
+    marginLeft: 0,
+    marginTop: 5,
+    marginBottom: 5,
+}
