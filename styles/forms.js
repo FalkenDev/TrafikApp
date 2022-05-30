91429cd4d9568222429fd3dc20d9d13bbc2fbeb0
“FalkenDev” <kafa21@student.bth.se>
2022-05-30 07:30:46 +0100

    Added styles for forms

---
+++ styles/forms.js
@@ -0,0 +1,13 @@
+export const formView = {
+    backgroundColor: "#000000",
+};
+
+export const input = {
+    fontSize: 20,
+    marginBottom: 28,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "#ccc",
+    borderRadius: 3,
+    color: "#fff",
+};
