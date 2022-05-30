f085ff17df8e5967a54e72841cbda8f043d86d1a
“FalkenDev” <kafa21@student.bth.se>
2022-05-30 07:42:43 +0100

    Added trafficInfo and setTrafficInfo to components from lift state up

--- components/trafficinfo/Traffic.tsx
+++ components/trafficinfo/Traffic.tsx
@@ -1,13 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import TrafficList from './TrafficList';
+import InfoList from './InfoList';
 
 const Stack = createNativeStackNavigator();
 
-export default function Pick(props) {
+export default function TrafficInfo(props) {
     return (
-        <Stack.Navigator initialRouteName="List">
-            <Stack.Screen name="List" component={TrafficList} />
+        <Stack.Navigator initialRouteName="Förseningar">
+            <Stack.Screen name="Förseningar" options={{headerStyle: {backgroundColor: '#1a1a1a'}, headerTintColor: '#fff', headerTitleStyle: {fontWeight: 'bold'}}}>
+                {(screenProps) => <TrafficList {...screenProps}
+                trafficInfo={props.trafficInfo} setTrafficInfo={props.setTrafficInfo} /*finalDestination={props.finalDestination} setFinalDestination={props.setFinalDestination}*//>}
+            </Stack.Screen>
+            <Stack.Screen name="Details" component={InfoList} options={{headerStyle: {backgroundColor: '#1a1a1a'}, headerTintColor: '#fff', headerTitleStyle: {fontWeight: 'bold'}}}/>
         </Stack.Navigator>
     );
 }
