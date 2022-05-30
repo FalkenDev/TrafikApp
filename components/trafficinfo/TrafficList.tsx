21609594faa8a519a7dd85299376a6ec30f89fd5
“FalkenDev” <kafa21@student.bth.se>
2022-05-30 07:41:05 +0100

    Updated trafficlist to sort and reload

--- components/trafficinfo/TrafficList.tsx
+++ components/trafficinfo/TrafficList.tsx
@@ -1,46 +1,80 @@
-import { useState, useEffect } from 'react';
-import { View, Text, Button } from "react-native";
+import { useEffect } from 'react';
+import { ScrollView, Text, Button, StyleSheet } from "react-native";
 import { Typography } from '../../styles/index.js';
-import orderModel from "../../models/traffic"
+import traffic from "../../models/traffic"
 
-export default function OrderList({ route, navigation }) {
-    console.log("------| OrderList |------")
-    const { reload } = route.params || true;
-    const [allOrders, setAllOrders] = useState([]);
+export default function OrderList({ route, navigation, trafficInfo, setTrafficInfo, finalDestination, setFinalDestination}) {
+    console.log("------| Traffic delays list |------")
+    let ifTrians = false;
+    let trainNumber = [];
 
-    if(reload) {
-        console.log("reloading time!");
-        reloadOrders();
-        route.params = false;
-    }
-
-    async function reloadOrders() {
-        setAllOrders(await orderModel.getOrders());
+    async function reloadTrafficInfo() {
+        console.log("Loading");
+        setTrafficInfo(await traffic.getTrafficInfo());
+        //setFinalDestination(await traffic.getFinalDestination());
+        //console.log("finalDestination");
+        //console.log(finalDestination);
     }
 
     useEffect(() => {
-        reloadOrders();
+        console.log("useEffect")
+        navigation.addListener('focus', () => reloadTrafficInfo());
     }, []);
 
-    const listOfOrders = allOrders
-        .filter(order => order.status === "Ny")
-        .map((order, index) => {
+
+    // Sorts alphabetically from title
+    function sortAlpha( a, b ) {
+        if ( a.props.title < b.props.title ){
+          return -1;
+        }
+        if ( a.props.title > b.props.title ){
+          return 1;
+        }
+        return 0;
+      }
+
+    // Get all delay trains, filter out all the same train number
+    const listOfDelays = trafficInfo
+        .map((item, index) => {
+            if(trainNumber.includes(item.AdvertisedTrainIdent)) {
+                return;
+            };
+            //let finalStation = finalDestination[index].AdvertisedLocationName;
+            trainNumber.push(item.AdvertisedTrainIdent);
             return(
             <Button
-                title={order.name}
+                color='#1a1a1a'
+                title={item.AdvertisedLocationName + " Tågnr " + item.AdvertisedTrainIdent}
                 key={index}
                 onPress={() => {
                     navigation.navigate('Details', {
-                        order: order
+                        item: item
                     });
                 }}
             />)
         });
 
+    // Sort the array of objects
+    listOfDelays.sort( sortAlpha )
+
+    if(listOfDelays.length === 0) {
+        ifTrians = true;
+    }
+
     return (
-        <View>
-            <Text style={Typography.header2}>Ordrar redo att plockas</Text>
-            {listOfOrders}
-        </View>
+        <ScrollView style={styles.button}>
+            {ifTrians
+                ?<Text style={Typography.warning}>Det finns inga tåg information just nu.</Text>
+                :<Text></Text>
+            }
+            {listOfDelays}
+            <Text></Text>
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    button: {
+      backgroundColor: 'black',
+    },
+  });
