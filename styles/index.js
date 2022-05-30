e8286c2b1e8877a2f5b096a050455f5a7d3a9519
“FalkenDev” <kafa21@student.bth.se>
2022-05-30 07:37:57 +0100

    Added more styles files in index

--- styles/index.js
+++ styles/index.js
@@ -1,4 +1,6 @@
 import * as Base from './base';
 import * as Typography from './typography';
+import * as TrafficInfoStyle from './trafficInfo';
+import * as Forms from './forms';
 
-export { Base, Typography };
+export { Base, Typography, TrafficInfoStyle, Forms };
