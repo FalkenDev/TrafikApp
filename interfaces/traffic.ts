014aad1456a022f0f37343228ca9b9d911e59b2e
“FalkenDev” <kafa21@student.bth.se>
2022-05-30 07:39:27 +0100

    Changed name

--- interfaces/traffic.ts
+++
@@ -1,8 +0,0 @@
-export default interface Delays {
-    ActivityId: string,
-    ActivityType: string,
-    AdvertisedTimeAtLocation: string,
-    AdvertisedTrainIdent: string,
-    Canceled: boolean,
-    EstimatedTimeAtLocation: string
-};
