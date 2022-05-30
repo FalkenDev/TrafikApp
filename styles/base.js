dd96c6cfc557dc713dbe8554b0e32ef59dfea2d1
“FalkenDev” <kafa21@student.bth.se>
2022-05-30 07:38:12 +0100

    Updated base styles

--- styles/base.js
+++ styles/base.js
@@ -1,3 +1,5 @@
+import { color } from "./typography";
+
 export const container = {
     flex: 1,
     backgroundColor: '#1a1a1a',
@@ -21,4 +23,52 @@ export const base = {
     backgroundColor: '#fff',
     paddingLeft: 12,
     paddingRight: 12,
+}
+
+export const input = {
+    fontSize: 20,
+    marginBottom: 28,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "#ccc",
+    borderRadius: 3,
+};
+
+export const button = {
+    fontSize: 20,
+    marginBottom: 28,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "#ccc",
+    borderRadius: 3,
+};
+
+export const details = {
+    backgroundColor: "#000000",
+    color: "#fff"
+}
+
+export const detailsText = {
+    color: "#fff",
+    marginLeft: 10,
+    marginTop: 10,
+    fontSize: 15,
+};
+
+export const detailsTextWarning = {
+    color: "#FF0000",
+    marginLeft: 10,
+    marginTop: 10,
+    fontSize: 15,
+};
+
+export const callOutView = {
+    borderWidth: 6,
+    backgroundColor: "#1a1a1a",
+    borderColor: "#fff",
+}
+
+export const callOutText = {
+    padding:10,
+    color: "#fff",
 }
