d20ff959e9f5a47e8435f609cd9f396f32ad4915
“FalkenDev” <kafa21@student.bth.se>
2022-05-30 07:41:42 +0100

    Updated app.tsx with liftstateup

--- App.tsx
+++ App.tsx
@@ -4,19 +4,41 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Base } from './styles/index.js'; 
+import { useEffect, useState } from 'react';
 import FlashMessage from 'react-native-flash-message';
 import Home from './components/Home';
-
+import Favorite from './components/favorite/Favorite';
+import TrafficInfo from './components/trafficinfo/Traffic';
+import Map from './components/map/Map';
+import traffic from './models/traffic';
+import Auth from './components/auth/Auth';
+import authModel from "./models/auth";
 
 const Tab = createBottomTabNavigator();
 const routeIcons = {
   "Hem": "home",
   "Stationer":"transfer-within-a-station",
   "Trafikinfo":"train",
+  "Karta":"map",
+  "Logga in":"login",
 };
 
 export default function App() {
   console.log("------| app.tsx |------");
+  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(false);
+  const [trafficInfo, setTrafficInfo] = useState([]);
+  const [favorite, setFavorite] = useState([]);
+  //const [finalDestination, setFinalDestination] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      console.log("Getting login");
+      setIsLoggedIn(await authModel.loggedIn());
+      setTrafficInfo(await traffic.getTrafficInfo());
+      //setFinalDestination(await traffic.getFinalDestination());
+    })();
+}, []);
+
   return (
     <SafeAreaView style={Base.container}>
       <NavigationContainer>
@@ -49,8 +71,20 @@ export default function App() {
       })}
     >
           <Tab.Screen name="Hem" component={Home}/>
-          <Tab.Screen name="Stationer" component={Home}/>
-          <Tab.Screen name="Trafikinfo" component={Home}/>
+          <Tab.Screen name="Trafikinfo">
+            {() => <TrafficInfo trafficInfo={trafficInfo} setTrafficInfo={setTrafficInfo} /*finalDestination={finalDestination} setFinalDestination={setFinalDestination}*//>}
+          </Tab.Screen>
+          <Tab.Screen name="Karta">
+            {() => <Map trafficInfo={trafficInfo}/>}
+          </Tab.Screen>
+          {isLoggedIn
+            ?<Tab.Screen name="Stationer">
+              {() => <Favorite favorite={favorite} setFavorite={setFavorite} setIsLoggedIn={setIsLoggedIn} trafficInfo={trafficInfo} setTrafficInfo={setTrafficInfo}/>}
+            </Tab.Screen>
+            :<Tab.Screen name="Logga in">
+              {() => <Auth setIsLoggedIn={setIsLoggedIn} />}
+            </Tab.Screen>
+          }
         </Tab.Navigator>
       </NavigationContainer>
       <StatusBar style="light" />
