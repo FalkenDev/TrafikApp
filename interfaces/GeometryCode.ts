82016c4d33749a9451e96b1a1bba3be74e1f7a3c
“FalkenDev” <kafa21@student.bth.se>
2022-05-30 07:40:11 +0100

    Changed name

--- interfaces/GeometryCode.ts
+++
@@ -1,7 +0,0 @@
-import GeometryCode from "./stations_geo"
-export default interface Stations {
-    AdvertisedLocationName: string,
-    Geometry: Array<GeometryCode>,
-    LocationSignature: string,
-    PlatformLine: Array<string>
-};
