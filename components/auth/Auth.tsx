2540ee71a35bb9d04aaf87cd48449c35897739d3
“FalkenDev” <kafa21@student.bth.se>
2022-05-31 08:11:37 +0100

    Updated fetch on register

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -4,7 +4,7 @@ import Register from './Register';
 
 const Stack = createNativeStackNavigator();
 
-export default function Deliveries(props) {
+export default function Auth(props) {
     return (
         <Stack.Navigator initialRouteName='Login'>
             <Stack.Screen name='Login' options={{headerStyle: {backgroundColor: '#1a1a1a'}, headerTintColor: '#fff', headerTitleStyle: {fontWeight: 'bold'}}}>
